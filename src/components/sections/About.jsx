a24d5b2773e3cd8625d56b8dc0bad056323db8eb
dinitha-cyber <dinithavalo1@gmail.com>
2025-03-28 14:19:29 -0700

    fix: Animation issue

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
         id="about" 
         className="min-h-screen flex items-center justify-center py-20"
     >
-        <RevealOnScroll>
+        
 
         <div className="max-w-3xl mx-auto px-4">
             <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"> About Me</h2>
@@ -87,7 +87,7 @@ roles to drive innovation while expanding hands-on experience.
                 </div>
             </div>
         </div>
-        </RevealOnScroll>
+        
     </section>
     );
 };
